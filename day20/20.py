2642535ff25ea3cf1455267dfed08b7656369f91
Stef Smeets <s.smeets@esciencecenter.nl>
2021-12-20 10:26:56 +0100

    Day 20 clean code

--- day20/20.py
+++ day20/20.py
@@ -6,31 +6,25 @@ filename = 'data.txt'
 with open(filename) as f:
     lines = (line.strip() for line in f.readlines())
 
-mapping = {'.': 0, '#': 1}
+enhance = np.array([(char== '#') for char in next(lines)], dtype=np.int8)
 
-enhance = [mapping[char] for char in next(lines)]
 next(lines)
 
-im = np.array([[mapping[char] for char in line] for line in lines])
+im = np.array([[(char == '#') for char in line] for line in lines], dtype=np.int8)
+im = np.pad(im, 51)
 
 
 def f(x):
-    idx = int('0b' + ''.join([str(val) for val in x.astype(int)]), 2)
+    # https://stackoverflow.com/a/15506055
+    idx = x.astype(np.int8).dot(1 << np.arange(x.shape[-1] - 1, -1, -1))
     return enhance[idx]
 
 
 def step(im, steps):
-    cval = 0
-
     for step in range(steps):
-        im = np.pad(im, 1, constant_values=cval)
         im = ndimage.generic_filter(im, f, size=(3,3))
-        cval = enhance[[0,-1][cval]]
-
     return im
 
 
 print(f'part 1: {step(im, 2).sum()=}')
 print(f'part 2: {step(im, 50).sum()=}')
-
-breakpoint()
